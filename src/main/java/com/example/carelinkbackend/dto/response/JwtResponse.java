81a46dd1277819dcaec61e17ad52cbd692001d80
Sandaru Bandara <sandarubandara96@gmail.com>
2025-03-31 15:47:55 -0700

    dto

---
+++ src/main/java/com/example/carelinkbackend/dto/response/JwtResponse.java
@@ -0,0 +1,58 @@
+package com.example.carelinkbackend.dto.response;
+
+import java.util.List;
+
+public class JwtResponse {
+
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private List<String> roles;
+
+    public JwtResponse(String token, Long id, String username, String email, 
+                      String firstName, String lastName, List<String> roles) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
---
+++ src/main/java/com/example/carelinkbackend/dto/response/TokenRefreshResponse.java
@@ -0,0 +1,25 @@
+package com.example.carelinkbackend.dto.response;
+
+public class TokenRefreshResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+
+    public TokenRefreshResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+}
